d9087e39f5d63f7803955a0445ad4a92006452af
MR | SACHIYA MD <148801649+MR-SACHIYA1@users.noreply.github.com>
2023-10-26 19:25:02 +0530

    Update Telegraph.js

--- commands /Telegraph.js
+++ commands /Telegraph.js
@@ -1,12 +1,12 @@
 
-
 /**
  Copyright (C) 2022.
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
  * @project_name : BLACK-HUNTER
- * @description : BLACK-HUNTER,A Multi-functional whatsapp bot.
+ * @author : MR-SACHIYA <https://github.com/MR-SACHIYA1>
+ * @description : Secktor,A Multi-functional whatsapp bot.
  * @version 0.0.1
  **/
 //---------------------------------------------------------------------------
