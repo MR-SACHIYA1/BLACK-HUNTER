466bd99ff51b88f7da5f1a78990f19f4419089ab
KALIDUWA OFC ✔️ <132440655+MR-KALINDU@users.noreply.github.com>
2023-10-27 07:24:53 +0530

    BY @MR-KALINDU

--- commands /downloader.js
+++ commands /downloader.js
@@ -111,7 +111,7 @@ cmd({
                 if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
                 let titleYt = infoYt.videoDetails.title;
                 let randomName = getRandom(".mp4");
-                citel.reply('*Downloadig:* '+titleYt)
+                citel.reply('*YOUR VIDEO DOANLOADING BY BLACK-HUNTER BOT:* '+titleYt)
                 const stream = ytdl(urlYt, {
                         filter: (info) => info.itag == 22 || info.itag == 18,
                     })
@@ -301,7 +301,7 @@ cmd({
             if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
             let titleYt = infoYt.videoDetails.title;
             let randomName = getRandom(".mp3");
-            citel.reply('*Downloadig:* '+titleYt)
+            citel.reply('*YOUR SONG DOWNLOADING BY BLACK-HUNTER BOT:* '+titleYt)
             const stream = ytdl(anu.url, {
                     filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
                 })
